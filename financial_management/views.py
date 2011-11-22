d9fe9b5ebe1c805d6d434ac2b63353b8ff6e1bc2
ashish <ashishiitp@gmail.com>
2011-11-22 18:43:39 +0530

    Version_1.2 pair programmed change done in view.py  by Ashish and Rahul

--- financial_management/views.py
+++ financial_management/views.py
@@ -9,38 +9,120 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.http import HttpResponse
 
+user_name = None
+user_id1 = None
+tot_bal1= None
+mf_bal1= None
+po_bal1= None
+fo_bal1= None
+nw_bal1= None
+
+#def print_vars():
+#    print user_name
+#    print user_id1
+#    print tot_bal1
+#    print mf_bal1
+#    print po_bal1
+#    print fo_bal1
+#    print nw_bal1
+
 def login_page(request):
     return render_to_response("login.html")
 
-
 def account_detail(request):
-    user_id = request.POST["user_id"]
-    try:
-        user = account.objects.get(pk=user_id)
-        customer = Customer.objects.get(pk=user_id)
-        user_name = customer.customer_Name
-        user_id1 = user.user_id
-        tot_bal1= user.tot_bal
-        mf_bal1= user.mf_bal
-        po_bal1= user.po_bal
-        fo_bal1= user.fo_bal
-        return render_to_response("accountpage.html",{"user_id1":user_id1 ,  "mf_bal1":mf_bal1 , "tot_bal1":tot_bal1 , "po_bal1":po_bal1 ,"fo_bal1": fo_bal1, "user_name":user_name})
-    except account.DoesNotExist:
-        return HttpResponseRedirect("/cooperative_Bank/")
-
+	if 'temp_MF' in request.POST and request.POST['temp_MF']:
+		user_id = request.POST["user_id"]
+		temp_mf = request.POST["temp_MF"]
+		temp_po = request.POST["temp_PO"]
+		temp_fo = request.POST["temp_FO"]
+		print(user_id)
+		print(temp_mf)
+		try:
+			
+			global user_name
+			global user_id1 
+			global tot_bal1
+			global mf_bal1
+			global po_bal1
+			global fo_bal1
+			global nw_bal1
+			
+			user = account.objects.get(pk=user_id)
+			customer = Customer.objects.get(pk=user_id)
+			user_name = customer.customer_Name
+			user_id1 = user.user_id
+			tot_bal1= int(user.tot_bal)
+			mf_bal1= int(user.mf_bal) +int(temp_mf)
+			po_bal1= int(user.po_bal)+int(temp_po)
+			fo_bal1= int(user.fo_bal)+int(temp_fo)
+			nw_bal1 =(tot_bal1)-(mf_bal1+po_bal1+fo_bal1)
+		  # print_vars()
+			return render_to_response("accountpage.html",{"user_id1":user_id1 ,  "mf_bal1":mf_bal1 , "tot_bal1":tot_bal1 , "po_bal1":po_bal1 ,"fo_bal1": fo_bal1, "user_name":user_name,"nw_bal1":nw_bal1})
+		except account.DoesNotExist:
+			return HttpResponseRedirect("/cooperative_Bank/")
+ 
+		
+	else:
+		user_id = request.POST["user_id"]
+		try:
+			
+			global user_name
+			global user_id1 
+			global tot_bal1
+			global mf_bal1
+			global po_bal1
+			global fo_bal1
+			global nw_bal1
+			
+			user = account.objects.get(pk=user_id)
+			customer = Customer.objects.get(pk=user_id)
+			user_name = customer.customer_Name
+			user_id1 = user.user_id
+			tot_bal1= int(user.tot_bal)
+			mf_bal1= int(user.mf_bal)
+			po_bal1= int(user.po_bal)
+			fo_bal1= int(user.fo_bal)
+			nw_bal1 =(tot_bal1)-(mf_bal1+po_bal1+fo_bal1)
+		  # print_vars()
+			return render_to_response("accountpage.html",{"user_id1":user_id1 ,  "mf_bal1":mf_bal1 , "tot_bal1":tot_bal1 , "po_bal1":po_bal1 ,"fo_bal1": fo_bal1, "user_name":user_name,"nw_bal1":nw_bal1})
+		except account.DoesNotExist:
+			return HttpResponseRedirect("/cooperative_Bank/")
  
 ##if view is MF show him MF page ,IPO show ipo.htm, FO fo.html else error
 def mf_detail(request):
-  mf_list = MutualFund.objects.all()
-  return render_to_response('mf.html',{'mf_list':mf_list})
+    global user_name
+    global user_id1 
+    global tot_bal1
+    global mf_bal1
+    global po_bal1
+    global fo_bal1
+    global nw_bal1
+    mf_list = MutualFund.objects.all()
+    return render_to_response('mf.html',{'mf_list':mf_list,"user_name":user_name ,"tot_bal1":tot_bal1 , "mf_bal1":mf_bal1 ,"nw_bal1":nw_bal1})
 
 def ipo_detail(request):
-  ipo_list = IPO.objects.all()
-  return render_to_response('ipo.html',{'ipo_list':ipo_list})
+    global user_name
+    global user_id1 
+    global tot_bal1
+    global mf_bal1
+    global po_bal1
+    global fo_bal1
+    global nw_bal1
+
+    ipo_list = IPO.objects.all()
+    return render_to_response('ipo.html',{'ipo_list':ipo_list,"user_name":user_name ,"tot_bal1":tot_bal1 , "po_bal1":po_bal1 ,"nw_bal1":nw_bal1,})
   
 def fo_detail(request):
-  fo_list = FO.objects.all()
-  return render_to_response('fo.html',{'fo_list':fo_list})
+    global user_name
+    global user_id1 
+    global tot_bal1
+    global mf_bal1
+    global po_bal1
+    global fo_bal1
+    global nw_bal1
+
+    fo_list = FO.objects.all()
+    return render_to_response('fo.html',{'fo_list':fo_list,"user_name":user_name ,"tot_bal1":tot_bal1 , "fo_bal1":fo_bal1 ,"nw_bal1":nw_bal1,})
 
 #mf_id = "MF2008X1";
   #mf = MutualFund.objects.get(pk=mf_id)
