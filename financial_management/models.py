340930c69708cbbb06c554fddf20a9ca7600f6d4
unknown <parry@.(none)>
2011-11-23 21:18:22 +0530

    Test Cases Written, and MF page validation

--- financial_management/models.py
+++ financial_management/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 import json
+#Customer who are identified by their id and name
 class Customer(models.Model):
   customer_Name = models.CharField(max_length=200)
   customer_ID = models.CharField(max_length=10, primary_key = True)
   def __unicode__(self):
         return self.customer_ID
-
+#For each customer there is an account along with all his bank details
 class account(models.Model):
     user_id =models.ForeignKey(Customer, primary_key = True)
     tot_bal =models.CharField(max_length=30)
@@ -13,7 +14,7 @@ class account(models.Model):
     po_bal =models.CharField(max_length=30)
     fo_bal =models.CharField(max_length=30)
 
-
+#list of mutual funds available along with their quantity available and cost.
 class MutualFund(models.Model):
   mf_ID = models.CharField(max_length=10, primary_key = True)
   mf_Name = models.CharField(max_length=200)
@@ -21,7 +22,11 @@ class MutualFund(models.Model):
   mf_Qty = models.IntegerField()
   def __unicode__(self):
         return self.mf_Name
-  
+  def mf(self): 
+      if self.mf_Qty<0:
+          self.mf_Qty=0
+          return self.mf_Qty
+#list of IPO available along with their quantity available and cost.  
 class IPO(models.Model):
   ipo_Name = models.CharField(max_length=200)
   ipo_ID = models.CharField(max_length=10, primary_key = True)
@@ -29,7 +34,11 @@ class IPO(models.Model):
   ipo_Qty = models.IntegerField()
   def __unicode__(self):
         return self.ipo_Name
-
+  def ipo(self): 
+      if self.ipo_Qty<0:
+          self.ipo_Qty=0
+          return self.ipo_Qty
+#list of FO available along with their quantity available and cost.      
 class FO(models.Model):
   fo_Name = models.CharField(max_length=200)
   fo_ID = models.CharField(max_length=10,primary_key = True)
@@ -37,7 +46,11 @@ class FO(models.Model):
   fo_Qty = models.IntegerField()
   def __unicode__(self):
         return self.fo_Name
-
+  def fo(self): 
+      if self.fo_Qty<0:
+          self.fo_Qty=0
+          return self.fo_Qty
+#All the transactions made with the bank by all the customers.Used to determine the transaction table for a particular customer.      
 class Transaction(models.Model):
   transaction_user = models.ForeignKey(Customer)
   transaction_id = models.CharField(max_length=10, primary_key = True)
--- financial_management/tests.py
+++ financial_management/tests.py
@@ -1,3 +1,12 @@
+from cooperative_Bank.financial_management.models import Customer
+from financial_management.models import MutualFund
+from financial_management.models import IPO
+from financial_management.models import FO
+from financial_management.models import Admin
+from financial_management.models import account
+from financial_management.models import Transaction
+from django.utils import unittest
+from django.test.client import Client
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -14,3 +23,51 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+    def test_redirect_after_login(self):
+        c = Client()
+        user = Customer('test1',1010)
+        acc = account('1010','13213','3213','321','5435')
+        user.save()
+        acc.save()
+        response = c.post('/cooperative_Bank/',{'user_id': 1010})
+        response.content
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual(response.status_code,200)
+      #  self.assertRedirects(response,'/cooperative_Bank/fm/user')
+    def test_mf_after_login(self):
+        c = Client()
+        user = Customer('test1',1010)
+        acc = account('1010','13213','3213','321','5435')
+        mf=MutualFund('2','mf',12,12321)
+        mf.save()
+        user.save()
+        acc.save()
+        response = c.post('/cooperative_Bank/fm/MF',{'abc1': '2','abcd1':'10'})
+        c.post('/cooperative_Bank/fm/MF', {'name': 'fred', 'passwd': 'secret'})
+        self.assertEqual(1 + 1, 2)
+        response.content
+       # self.assertEqual(response.status_code,302)
+
+class MutualFundTestCase(unittest.TestCase):
+    def test_mf1_after_login(self):
+        self.mf=MutualFund('2','mf',12,12321)
+        self.mf.save()
+        self.mf.mf_Qty=self.mf.mf_Qty-100000
+        k=self.mf.mf()
+        self.assertEqual(self.mf.mf_Qty, 0)
+
+class IPOTestCase(unittest.TestCase):
+    def test_mf1_after_login(self):
+        self.mf=IPO('2','mf',12,12321)
+        self.mf.save()
+        self.mf.ipo_Qty=self.mf.ipo_Qty-100000
+        k=self.mf.ipo()
+        self.assertEqual(self.mf.ipo_Qty, 0)  
+
+class FOTestCase(unittest.TestCase):
+    def test_mf1_after_login(self):
+        self.mf=FO('2','mf',12,12321)
+        self.mf.save()
+        self.mf.fo_Qty=self.mf.fo_Qty-100000
+        k=self.mf.fo()
+        self.assertEqual(self.mf.fo_Qty, 0)      
